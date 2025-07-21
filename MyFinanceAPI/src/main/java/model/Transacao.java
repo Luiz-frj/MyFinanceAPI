de9e6e7cd96f092fbab8bfefc00655d9982863ae
Luiz-frj <Luizfrj200412@gmail.com>
2025-07-21 18:22:48 -0300

    Criacao das classes

---
+++ MyFinanceAPI/src/main/java/model/Transacao.java
@@ -0,0 +1,78 @@
+package model;
+
+public class Transacao {
+
+	private int id;
+	private String descricao;
+	private double valor;
+	private String tipo;
+	private String categoria;
+	private String data_criacao;
+	
+	public Transacao() {}
+
+	public Transacao(int id, 
+			String descricao, 
+			double valor, 
+			String tipo, 
+			String categoria, 
+			String data_criacao) {
+		this.id = id;
+		this.descricao = descricao;
+		this.valor = valor;
+		this.tipo = tipo;
+		this.categoria = categoria;
+		this.data_criacao = data_criacao;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getData_criacao() {
+		return data_criacao;
+	}
+
+	public void setData_criacao(String data_criacao) {
+		this.data_criacao = data_criacao;
+	}
+	
+	
+	
+}
